17710f77a0993575c2fb08337da4ad1b4af9430b
Mahdi <amiralmasi021@gmail.com>
2024-03-26 01:08:54 +0330

    Add target blank for reaserch projects

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -17,197 +17,265 @@ const Demo = () => {
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:5Ul4iDaHHb8C"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Mobile User Indoor-Outdoor Detection Through Physical Daily
           Activities(2019)
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:fQNAKQ3IYiAC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           A machine learning based approach for counting blister cards within
           drug packages(2019)
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:738O_yMBCRsC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Vehicle speed estimation based on the image motion blur using radon
           transform(2010)
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:tOudhMTPpwUC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Designing quality control system based on vision inspection in
           pharmaceutical product lines(2018)
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:AXPGKjj_ei8C"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Using machine learning and computer vision to estimate the angular
           velocity of wind turbines in smart grids remotely(2021)
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:kRWSkSYxWN8C"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Wind turbine tower detection using feature descriptors and deep
           learning(2019)
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:V3AGJWp-ZtQC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Vision inspection of bottle caps in drink factories using
           convolutional neural networks(2019)
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:d1gkVwhDpl0C"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Novel approach for baseline detection and Text line segmentation(2012)
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:u-x6o8ySG0sC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Textdependent Speaker Recognition by combination of LBG VQ and DTW for
           persian language(2012)
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:UeHWp8X0CEIC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Fingerprint image enhancement using gabor wavelet transform(2010)
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:t6usbXjVLHcC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Estimation of wind turbine angular velocity remotely found on video
           mining and convolutional neural network(2020)
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:q3oQSFYPqjQC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Image Transmission Over Cognitive Radio Networks for Smart Grid
           Applications(2019)
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:WbkHhVStYXYC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Vision inspection and monitoring of wind turbine farms in
           emerging(2018) smart grids(2019)
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:OU6Ihb5iCvQC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Psnr enhancement in image streaming over cognitive radio sensor
           networks
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:9yKSN-GCB0IC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Legendre moments for face identification based on single image per
           person
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:2osOgNQ5qMEC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Fingerprint image enhancement using GWT and DMF
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:eMMeJKvmdy0C"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Optimum outlier detection in Internet of things industries using
           autoencoder
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:B3FOqHPlNUQC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           A new descriptor for UAV images mapping by applying discrete local
           radon
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&citation_for_view=0SXb4EsAAAAJ:tS2w5q8j5-wC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Design and implementation of fuzzy supervisor controller on optimized
           DC machine driver
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&cstart=20&pagesize=80&citation_for_view=0SXb4EsAAAAJ:Mojj43d5GZwC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Developing a parallel classifier for mining in big data sets
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&cstart=20&pagesize=80&citation_for_view=0SXb4EsAAAAJ:WA5NYHcadZ8C"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           A new anomalous text detection approach using unsupervised methods
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&cstart=20&pagesize=80&citation_for_view=0SXb4EsAAAAJ:N5tVd3kTz84C"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           A high-accuracy phishing website detection method based on machine
           learning
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&cstart=20&pagesize=80&citation_for_view=0SXb4EsAAAAJ:9vf0nzSNQJEC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Bitcoin daily close price prediction using optimized grid search
           method
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&cstart=20&pagesize=80&citation_for_view=0SXb4EsAAAAJ:JoZmwDi-zQgC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           A Time-Efficient and Exploratory Algorithm for the Rectangle Packing
           Problem.
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&cstart=20&pagesize=80&citation_for_view=0SXb4EsAAAAJ:dTyEYWd-f8wC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           A Tradeoff Between Accuracy and Speed for K-Means Seed Determination.
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&cstart=20&pagesize=80&citation_for_view=0SXb4EsAAAAJ:5awf1xo2G04C"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Consensus tracking for a class of fractional-order non-linear
           multi-agent systems via an adaptive dynamic surface controller
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&cstart=20&pagesize=80&citation_for_view=0SXb4EsAAAAJ:LjlpjdlvIbIC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Radon CLF: A Novel Approach for Skew Detection Using Radon Transform
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&cstart=20&pagesize=80&citation_for_view=0SXb4EsAAAAJ:PR6Y55bgFSsC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           A new weighting scheme for document ranking based on the modified
           word-embedding method
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&cstart=20&pagesize=80&citation_for_view=0SXb4EsAAAAJ:ZuybSZzF8UAC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Prediction of effective management of electronic banking in the ease
           of transmitting information using data mining techniques
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&cstart=20&pagesize=80&citation_for_view=0SXb4EsAAAAJ:VL0QpB8kHFEC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Textual outlier detection with an unsupervised method using text
           similarity and density peak
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&cstart=20&pagesize=80&citation_for_view=0SXb4EsAAAAJ:WqliGbK-hY8C"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Scientist Clustering-Index: Using Machine Learning Algorithms and
           Google Scholar Information to Cluster Scientists
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&cstart=20&pagesize=80&citation_for_view=0SXb4EsAAAAJ:ye4kPcJQO24C"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Review and comparison of image annotation software tools for big
           datasets in computer vision applications
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&cstart=20&pagesize=80&citation_for_view=0SXb4EsAAAAJ:nb7KW1ujOQ8C"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Analysis of the first European smart grids standards based on SGAM
           architecture
         </a>
         <a
           href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=0SXb4EsAAAAJ&cstart=20&pagesize=80&citation_for_view=0SXb4EsAAAAJ:xtRiw3GOFMkC"
+          target="_blank"
+          rel="noopener noreferrer"
           class="gsc_a_at">
           Fingerprint Image enhancement in three domains including spatial,
           frequency , and combined spatial-frequency domain and survey the usage
