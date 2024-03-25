5cd08547ac7a15fca922711b48eef7d032cd22d3
Mahdi <amiralmasi021@gmail.com>
2024-03-25 14:16:06 +0330

    Add atricles

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -1,21 +1,225 @@
-import React from 'react'
-import './Demo.css'
+import React from "react";
+import "./Demo.css";
 
 const Demo = () => {
-    return (
-        <div className='demo' id='demo'>
-            <div className='container'>
-                <div className='col-1'>
-                    <p>its layout. The point of using Lorem Ipsum is that it has</p>
-                    <p>One Philosophy</p>
-                    <p>make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets contain.</p>
-                    <button className='button'>Get your free financial analysis</button>
-                </div>
-                <div className='col-2'>
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="demo" id="demo">
+      <h4>List of articles</h4>
 
-export default Demo
+      <div className="px-6 grid grid-cols-2 gap-4 ">
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:5Ul4iDaHHb8C"
+          class="gsc_a_at">
+          Mobile User Indoor-Outdoor Detection Through Physical Daily
+          Activities(2020)
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:fQNAKQ3IYiAC"
+          class="gsc_a_at">
+          A machine learning based approach for counting blister cards within
+          drug packages
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:738O_yMBCRsC"
+          class="gsc_a_at">
+          Vehicle speed estimation based on the image motion blur using radon
+          transform
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:738O_yMBCRsC"
+          class="gsc_a_at">
+          Vehicle speed estimation based on the image motion blur using radon
+          transform
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:tOudhMTPpwUC"
+          class="gsc_a_at">
+          Designing quality control system based on vision inspection in
+          pharmaceutical product lines
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:AXPGKjj_ei8C"
+          class="gsc_a_at">
+          Using machine learning and computer vision to estimate the angular
+          velocity of wind turbines in smart grids remotely
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:kRWSkSYxWN8C"
+          class="gsc_a_at">
+          Wind turbine tower detection using feature descriptors and deep
+          learning
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:V3AGJWp-ZtQC"
+          class="gsc_a_at">
+          Vision inspection of bottle caps in drink factories using
+          convolutional neural networks
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:d1gkVwhDpl0C"
+          class="gsc_a_at">
+          Novel approach for baseline detection and Text line segmentation
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:u-x6o8ySG0sC"
+          class="gsc_a_at">
+          Textdependent Speaker Recognition by combination of LBG VQ and DTW for
+          persian language
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:UeHWp8X0CEIC"
+          class="gsc_a_at">
+          Fingerprint image enhancement using gabor wavelet transform
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:t6usbXjVLHcC"
+          class="gsc_a_at">
+          Estimation of wind turbine angular velocity remotely found on video
+          mining and convolutional neural network
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:q3oQSFYPqjQC"
+          class="gsc_a_at">
+          Image Transmission Over Cognitive Radio Networks for Smart Grid
+          Applications
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:WbkHhVStYXYC"
+          class="gsc_a_at">
+          Vision inspection and monitoring of wind turbine farms in emerging
+          smart grids
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:OU6Ihb5iCvQC"
+          class="gsc_a_at">
+          Psnr enhancement in image streaming over cognitive radio sensor
+          networks
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:OU6Ihb5iCvQC"
+          class="gsc_a_at">
+          Psnr enhancement in image streaming over cognitive radio sensor
+          networks
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:9yKSN-GCB0IC"
+          class="gsc_a_at">
+          Legendre moments for face identification based on single image per
+          person
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:2osOgNQ5qMEC"
+          class="gsc_a_at">
+          Fingerprint image enhancement using GWT and DMF
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:eMMeJKvmdy0C"
+          class="gsc_a_at">
+          Optimum outlier detection in Internet of things industries using
+          autoencoder
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:B3FOqHPlNUQC"
+          class="gsc_a_at">
+          A new descriptor for UAV images mapping by applying discrete local
+          radon
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;citation_for_view=0SXb4EsAAAAJ:tS2w5q8j5-wC"
+          class="gsc_a_at">
+          Design and implementation of fuzzy supervisor controller on optimized
+          DC machine driver
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;cstart=20&amp;pagesize=80&amp;citation_for_view=0SXb4EsAAAAJ:Mojj43d5GZwC"
+          class="gsc_a_at">
+          Developing a parallel classifier for mining in big data sets
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;cstart=20&amp;pagesize=80&amp;citation_for_view=0SXb4EsAAAAJ:WA5NYHcadZ8C"
+          class="gsc_a_at">
+          A new anomalous text detection approach using unsupervised methods
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;cstart=20&amp;pagesize=80&amp;citation_for_view=0SXb4EsAAAAJ:N5tVd3kTz84C"
+          class="gsc_a_at">
+          A high-accuracy phishing website detection method based on machine
+          learning
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;cstart=20&amp;pagesize=80&amp;citation_for_view=0SXb4EsAAAAJ:9vf0nzSNQJEC"
+          class="gsc_a_at">
+          Bitcoin daily close price prediction using optimized grid search
+          method
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;cstart=20&amp;pagesize=80&amp;citation_for_view=0SXb4EsAAAAJ:JoZmwDi-zQgC"
+          class="gsc_a_at">
+          A Time-Efficient and Exploratory Algorithm for the Rectangle Packing
+          Problem.
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;cstart=20&amp;pagesize=80&amp;citation_for_view=0SXb4EsAAAAJ:dTyEYWd-f8wC"
+          class="gsc_a_at">
+          A Tradeoff Between Accuracy and Speed for K-Means Seed Determination.
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;cstart=20&amp;pagesize=80&amp;citation_for_view=0SXb4EsAAAAJ:5awf1xo2G04C"
+          class="gsc_a_at">
+          Consensus tracking for a class of fractional-order non-linear
+          multi-agent systems via an adaptive dynamic surface controller
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;cstart=20&amp;pagesize=80&amp;citation_for_view=0SXb4EsAAAAJ:LjlpjdlvIbIC"
+          class="gsc_a_at">
+          Radon CLF: A Novel Approach for Skew Detection Using Radon Transform
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;cstart=20&amp;pagesize=80&amp;citation_for_view=0SXb4EsAAAAJ:PR6Y55bgFSsC"
+          class="gsc_a_at">
+          A new weighting scheme for document ranking based on the modified
+          word-embedding method
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;cstart=20&amp;pagesize=80&amp;citation_for_view=0SXb4EsAAAAJ:ZuybSZzF8UAC"
+          class="gsc_a_at">
+          Prediction of effective management of electronic banking in the ease
+          of transmitting information using data mining techniques
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;cstart=20&amp;pagesize=80&amp;citation_for_view=0SXb4EsAAAAJ:VL0QpB8kHFEC"
+          class="gsc_a_at">
+          Textual outlier detection with an unsupervised method using text
+          similarity and density peak
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;cstart=20&amp;pagesize=80&amp;citation_for_view=0SXb4EsAAAAJ:WqliGbK-hY8C"
+          class="gsc_a_at">
+          Scientist Clustering-Index: Using Machine Learning Algorithms and
+          Google Scholar Information to Cluster Scientists
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;cstart=20&amp;pagesize=80&amp;citation_for_view=0SXb4EsAAAAJ:ye4kPcJQO24C"
+          class="gsc_a_at">
+          Review and comparison of image annotation software tools for big
+          datasets in computer vision applications
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;cstart=20&amp;pagesize=80&amp;citation_for_view=0SXb4EsAAAAJ:nb7KW1ujOQ8C"
+          class="gsc_a_at">
+          Analysis of the first European smart grids standards based on SGAM
+          architecture
+        </a>
+        <a
+          href="/citations?view_op=view_citation&amp;hl=en&amp;user=0SXb4EsAAAAJ&amp;cstart=20&amp;pagesize=80&amp;citation_for_view=0SXb4EsAAAAJ:xtRiw3GOFMkC"
+          class="gsc_a_at">
+          Fingerprint Image enhancement in three domains including spatial,
+          frequency , and combined spatial-frequency domain and survey the usage
+          of STFT for enhancing fingerprint images
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Demo;
